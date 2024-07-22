c9e886e7065d867156584df05f7b4f620063ae18
Nazar Mykhailenko <nazarprogramming@gmail.com>
2024-07-22 17:19:50 +0300

    Login page ui fix

--- src/UI/Input/Input.jsx
+++ src/UI/Input/Input.jsx
@@ -1,12 +1,21 @@
-import cls from './Input.module.css';
+import React from 'react'
+import cls from './Input.module.css'
 
 const Input = ({ id, label, ...props }) => {
 	return (
 		<div className={cls.inputCont}>
-			<label htmlFor={id}>{label}</label>
-			<input type='text' id={id} {...props} />
+			<input
+				type='text'
+				id={id}
+				className={cls.input}
+				placeholder=' '
+				{...props}
+			/>
+			<label htmlFor={id} className={cls.label}>
+				{label}
+			</label>
 		</div>
-	);
-};
+	)
+}
 
-export default Input;
+export default Input
--- src/modules/Login/components/LoginForm/LoginForm.jsx
+++ src/modules/Login/components/LoginForm/LoginForm.jsx
@@ -1,49 +1,71 @@
-import { useState } from 'react';
-import Button from '../../../../UI/Button/Button';
-import Input from '../../../../UI/Input/Input';
+import { useState } from 'react'
+import Button from '../../../../UI/Button/Button'
+import Input from '../../../../UI/Input/Input'
+import { Icon } from '@iconify/react/dist/iconify.js'
 
-import cls from './LoginForm.module.css';
-import { useMutation } from 'react-query';
-import { login } from '../../api/login';
-import { Link, useNavigate } from 'react-router-dom';
+import cls from './LoginForm.module.css'
+import { useMutation } from 'react-query'
+import { login } from '../../api/login'
+import { Link, useNavigate } from 'react-router-dom'
 
 const LoginForm = () => {
-	const [email, setEmail] = useState('');
-	const [password, setPassword] = useState('');
-	const navigate = useNavigate();
+	const [email, setEmail] = useState('')
+	const [password, setPassword] = useState('')
+	const navigate = useNavigate()
 
 	const { mutate } = useMutation({
 		mutationFn: login,
 		onSuccess: (result) => {
-			localStorage.setItem('token', result.data.access_token);
-			navigate('/profile/orders');
+			localStorage.setItem('token', result.data.access_token)
+			navigate('/profile/orders')
 		},
-	});
+	})
 
 	function handleLogin(e) {
-		e.preventDefault();
-		mutate({ email, password });
+		e.preventDefault()
+		mutate({ email, password })
 	}
 
 	return (
 		<form className={cls.form} onSubmit={handleLogin}>
-			<h2>Login to your account!</h2>
-			<Input
-				id='email'
-				label='Email'
-				value={email}
-				onChange={(e) => setEmail(e.target.value)}
-			/>
-			<Input
-				id='Password'
-				label='Password'
-				value={password}
-				onChange={(e) => setPassword(e.target.value)}
-			/>
-			<Button>Login</Button>
-			<Link to={'/register'}>Don't have an account? Register</Link>
-		</form>
-	);
-};
+			<div className={cls.title}>
+				<div>
+					<Link to='/register'>
+						<Icon
+							icon='mdi:chevron-left'
+							width='25px'
+							height='25px'
+							color='#000464'
+						/>
+					</Link>
+					<h2>Вход в аккаунт</h2>
+				</div>
+			</div>
 
-export default LoginForm;
+			<div className={cls.inputs}>
+				<Input
+					id='email'
+					label='Введите email'
+					value={email}
+					onChange={(e) => setEmail(e.target.value)}
+				/>
+				<Input
+					id='Password'
+					label='Пароль'
+					value={password}
+					onChange={(e) => setPassword(e.target.value)}
+				/>
+				<Button>Продолжить</Button>
+			</div>
+
+			<div className={cls.resetLinkBlock}>
+				<div>
+					<div>Забыли пароль? </div>
+					<Link to='/reset-password'>Восстановить пароль</Link>
+				</div>
+			</div>
+		</form>
+	)
+}
+
+export default LoginForm
