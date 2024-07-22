8a74439bf8fbad525bab1ab36a7c3211e7df1187
Nazar Mykhailenko <nazarprogramming@gmail.com>
2024-07-22 17:30:56 +0300

    Register page ui fix

--- src/modules/Register/components/RegisterForm/RegisterForm.jsx
+++ src/modules/Register/components/RegisterForm/RegisterForm.jsx
@@ -1,54 +1,84 @@
-import React, { useState } from 'react';
-import { useMutation } from 'react-query';
-import { register } from '../../api/register';
-import { Link } from 'react-router-dom';
-import Input from '../../../../UI/Input/Input';
-import Button from '../../../../UI/Button/Button';
+import React, { useState } from 'react'
+import { useMutation } from 'react-query'
+import { register } from '../../api/register'
+import { Link } from 'react-router-dom'
+import Input from '../../../../UI/Input/Input'
+import Button from '../../../../UI/Button/Button'
+import { Icon } from '@iconify/react/dist/iconify.js'
 
-import cls from './RegisterForm.module.css';
+import cls from './RegisterForm.module.css'
 
 const RegisterForm = () => {
-	const [email, setEmail] = useState('');
-	const [password, setPassword] = useState('');
-	const [username, setUsername] = useState('');
+	const [email, setEmail] = useState('')
+	const [password, setPassword] = useState('')
+	const [username, setUsername] = useState('')
 
 	const { mutate } = useMutation({
 		mutationFn: register,
 		onSuccess: (data) => {
-			console.log(data);
+			console.log(data)
 		},
-	});
+	})
 
 	function handleRegister(e) {
-		e.preventDefault();
-		mutate({ email, password, name: username });
+		e.preventDefault()
+		mutate({ email, password, name: username })
 	}
 
 	return (
 		<form className={cls.form} onSubmit={handleRegister}>
-			<h2>Register a new account!</h2>
-			<Input
-				id='email'
-				label='Email'
-				value={email}
-				onChange={(e) => setEmail(e.target.value)}
-			/>
-			<Input
-				id='name'
-				label='Name'
-				value={username}
-				onChange={(e) => setUsername(e.target.value)}
-			/>
-			<Input
-				id='Password'
-				label='Password'
-				value={password}
-				onChange={(e) => setPassword(e.target.value)}
-			/>
-			<Button>Register</Button>
-			<Link to={'/'}>Already have an account? Sign in</Link>
-		</form>
-	);
-};
+			<div className={cls.title}>
+				<div>
+					<Link to='/'>
+						<Icon
+							icon='mdi:chevron-left'
+							width='25px'
+							height='25px'
+							color='#000464'
+						/>
+					</Link>
+					<h2>Регистрация</h2>
+				</div>
+			</div>
 
-export default RegisterForm;
+			<div className={cls.inputs}>
+				<Input
+					id='email'
+					label='Введите email'
+					Введите
+					email
+					value={email}
+					onChange={(e) => setEmail(e.target.value)}
+				/>
+				<Input
+					id='name'
+					label='Введите ФИО'
+					value={username}
+					onChange={(e) => setUsername(e.target.value)}
+				/>
+				<Input
+					id='Password'
+					label='Пароль'
+					value={password}
+					onChange={(e) => setPassword(e.target.value)}
+				/>
+				{/* <Input
+					id='Password'
+					label='Подтверждение пароля'
+					value={password}
+					onChange={(e) => setPassword(e.target.value)}
+				/> */}
+				<Button>Register</Button>
+			</div>
+
+			<div className={cls.alreadySignedUpBlock}>
+				<div>
+					<div>Уже зарегистрировались?</div>
+					<Link to='/'>Войти</Link>
+				</div>
+			</div>
+		</form>
+	)
+}
+
+export default RegisterForm
--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -1,15 +1,18 @@
 import React from 'react'
-import cls from './Register.module.css'
 import RegisterForm from '../../modules/Register/components/RegisterForm/RegisterForm'
+import cls from './Register.module.css'
+import { Header } from '../../UI/Header/Header'
 
 const Register = () => {
-  return (
-    <div className='wrapper'>
+	return (
+		<div className='wrapper'>
+			<Header />
+
 			<div className={cls.registerFormCont}>
 				<RegisterForm />
 			</div>
 		</div>
-  )
+	)
 }
 
 export default Register
