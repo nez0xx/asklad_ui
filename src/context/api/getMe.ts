4b79582c464a1ff98abf594cadbd93ad8d5b3e36
Kot Fil <filippkotenko@outlook.com>
2024-08-26 10:55:37 +0300

    footer padding container

--- src/context/api/getMe.ts
+++ src/context/api/getMe.ts
@@ -1,7 +1,5 @@
 import AuthService from '../../services/AuthService'
 
 export async function getMe() {
-	const response = await AuthService.getMe()
-
-	return response.data
+	 return await AuthService.getMe()
 }
