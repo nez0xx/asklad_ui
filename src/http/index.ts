6832e6b124d98f4f78e5f46816bc44a26797888e
Kot Fil <filippkotenko@outlook.com>
2024-08-29 13:36:23 +0300

    remove refresh token api

--- src/http/index.ts
+++ src/http/index.ts
@@ -22,27 +22,5 @@ $api.interceptors.request.use(
 	}
 )
 
-$api.interceptors.response.use(
-	(response) => response,
-	async (error) => {
-		const originalRequest = error.config
-
-		if (error.response.status === 401 && !originalRequest._retry) {
-			originalRequest._retry = true
-			try {
-				const newAccessToken = await getRefreshToken()
-				localStorage.setItem('token', newAccessToken)
-
-				originalRequest.headers.Authorization = `Bearer ${newAccessToken}`
-				return $api(originalRequest)
-			} catch (refreshError) {
-				console.error('Token refresh failed', refreshError)
-				return Promise.reject(refreshError)
-			}
-		}
-
-		return Promise.reject(error)
-	}
-)
 
 export default $api
