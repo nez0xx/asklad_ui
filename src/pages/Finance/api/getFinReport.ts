d538b02755be231ddb1fca887f8d7c4e95ff4fa8
nez0xx <chugainov256@gmail.com>
2025-04-24 19:21:14 +0500

    fix finance table

--- src/pages/Finance/api/getFinReport.ts
+++ src/pages/Finance/api/getFinReport.ts
@@ -1,7 +1,15 @@
 import OrdersService from '@services/OrdersService'
 
 export async function getFinReport(date_min, date_max) {
-	const response = await OrdersService.getFinReport(date_min, date_max)
-
-	return response.data
+	const response = await OrdersService.getFinReport(date_min, date_max);
+	if (!response.data || Object.keys(response.data).length === 0) {
+	  return {
+		"total_price": {
+		  "rub": 0
+		},
+		"total_pv": 0,
+		"orders": {}
+	  };
+	}
+	return response.data;
 }
