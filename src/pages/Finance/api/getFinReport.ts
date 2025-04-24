c11878351d6bfc6031746f3276305472f0cf193b
nez0xx <chugainov256@gmail.com>
2025-04-24 19:28:58 +0500

    finance fix

--- src/pages/Finance/api/getFinReport.ts
+++ src/pages/Finance/api/getFinReport.ts
@@ -1,15 +1,26 @@
 import OrdersService from '@services/OrdersService'
 
 export async function getFinReport(date_min, date_max) {
-	const response = await OrdersService.getFinReport(date_min, date_max);
-	if (!response.data || Object.keys(response.data).length === 0) {
-	  return {
-		"total_price": {
-		  "rub": 0
-		},
-		"total_pv": 0,
-		"orders": {}
-	  };
+	try{
+		const response = await OrdersService.getFinReport(date_min, date_max);
+		if (!response.data || Object.keys(response.data).length === 0) {
+	  	return {
+			"total_price": {
+		  	"rub": 0
+			},
+			"total_pv": 0,
+			"orders": {}
+	  	};
+		}
+		return response.data;
+	} catch(error){
+		return {
+			"total_price": {
+		  	"rub": 0
+			},
+			"total_pv": 0,
+			"orders": {}
+	  	};
 	}
-	return response.data;
+	
 }
